566545f416ef6b037c198ff744e47bf441dc3c4e
irshad <irshadvtl272@gmail.com>
2023-09-10 16:38:25 +0530

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Service from './routes/Service';
+import Contact from './routes/Contact';
+import Notfound from './routes/Not_found';
 
 const App = () => {
   return (
     <div className='App'>
-      <Router>
+      
         <Navbar />
-      </Router>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path="*" element={<Notfound />} />
+        </Routes>
+ 
     </div>
   )
 }
---
+++ src/routes/Not_found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Notfound() {
+  return (
+    <div>
+       443 this page not found
+    </div>
+  )
+}
+
+export default Notfound
